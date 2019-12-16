08a942d2e3c8f3566b03328a5fbe6a3b7327fd6b
jd-a <16205186+jd-a@users.noreply.github.com>
2019-12-16 16:49:56 +0100

    Update cairo-withx.rb

--- Formula/cairo-withx.rb
+++ Formula/cairo-withx.rb
@@ -1,5 +1,5 @@
 class CairoWithx < Formula
-  desc "Vector graphics library with cross-device output support built with X"
+  desc "Vector graphics library with cross-device output support built with X11 support"
   homepage "https://cairographics.org/"
   url "https://cairographics.org/releases/cairo-1.16.0.tar.xz"
   sha256 "5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331"
