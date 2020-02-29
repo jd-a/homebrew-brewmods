0ac83cbe0748c78ae70cbd9e8319b55cd5b6d938
jd-a <16205186+jd-a@users.noreply.github.com>
2020-02-29 09:21:12 +0100

    Change checksum for 3.6.3
    second revision of 3.6.3

--- Formula/r-extendedcapabilities.rb
+++ Formula/r-extendedcapabilities.rb
@@ -2,7 +2,7 @@ class RExtendedcapabilities < Formula
   desc "Software environment for statistical computing with extended capabilities"
   homepage "https://www.r-project.org/"
   url "http://cran.r-project.org/src/base/R-3/R-3.6.3.tar.gz"
-  sha256 "bd65a45cddfb88f37370fbcee4ac8dd3f1aebeebe47c2f968fd9770ba2bbc954"
+  sha256 "89302990d8e8add536e12125ec591d6951022cf8475861b3690bc8bf1cefaa8f"
 
   depends_on :x11 # X11 for Tcl-Tk
   depends_on "pkg-config" => :build
