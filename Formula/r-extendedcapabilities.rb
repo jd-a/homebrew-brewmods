1b9493bc812ad135d5de4156b925428fd2a360cc
jd-a <16205186+jd-a@users.noreply.github.com>
2020-04-24 14:08:24 +0200

    Update to version 4

--- Formula/r-extendedcapabilities.rb
+++ Formula/r-extendedcapabilities.rb
@@ -1,9 +1,8 @@
 class RExtendedcapabilities < Formula
   desc "Software environment for statistical computing with extended capabilities"
   homepage "https://www.r-project.org/"
-  url "http://cran.r-project.org/src/base/R-3/R-3.6.3.tar.gz"
-  sha256 "89302990d8e8add536e12125ec591d6951022cf8475861b3690bc8bf1cefaa8f"
-  revision 3
+  url "http://cran.r-project.org/src/base/R-4/R-4.0.0.tar.gz"
+  sha256 "06beb0291b569978484eb0dcb5d2339665ec745737bdfb4e873e7a5a75492940"
 
   depends_on :x11 # X11 for Tcl-Tk
   depends_on "pkg-config" => :build
