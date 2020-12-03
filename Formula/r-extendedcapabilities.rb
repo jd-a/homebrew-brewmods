3f73352b7227252acd78523609155e03d7032541
jd-a <16205186+jd-a@users.noreply.github.com>
2020-12-03 12:45:49 +0100

    Update r-extendedcapabilities.rb

--- Formula/r-extendedcapabilities.rb
+++ Formula/r-extendedcapabilities.rb
@@ -16,7 +16,7 @@ class RExtendedcapabilities < Formula
   depends_on "xz"
 
   ## Add additional R capabilities (comment out if undesired)
-  depends_on :openjdk@11 => :optional
+  depends_on "openjdk" => :optional
   depends_on :x11 # X11 necessary for tcl-tk since tk.h includes X11 headers. See section A.2.1 Tcl/Tk at < https://cran.r-project.org/doc/manuals/r-release/R-admin.html >
   depends_on "texinfo" => :optional
   depends_on "libtiff" => :optional
