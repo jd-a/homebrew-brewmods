2f3602440b5ed14a00f8c809d4225281fdafe9f1
jd-a <16205186+jd-a@users.noreply.github.com>
2020-12-22 23:50:15 +0100

    Update cairo-withx.rb

--- Formula/cairo-withx.rb
+++ Formula/cairo-withx.rb
@@ -3,6 +3,13 @@ class CairoWithx < Formula
   homepage "https://cairographics.org/"
   url "https://cairographics.org/releases/cairo-1.16.0.tar.xz"
   sha256 "5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331"
+  revision 1
+
+  # bottle do
+  #   sha256 "6a23a68837269a8410a54950fdc8883feda091f221118370f1bfd3adbf5ee89c" => :catalina
+  #   sha256 "0984045234fb22fa3e54a337137e9e43a1bf997f5d77692ed02249dfdee2b1bf" => :mojave
+  #   sha256 "5c383ad4625fb1bd15e44e99fba1201490fa478b26178abaca5abb0fdb51510e" => :high_sierra
+  # end
 
   head do
     url "https://anongit.freedesktop.org/git/cairo", :using => :git
@@ -12,41 +19,55 @@ class CairoWithx < Formula
   end
 
   depends_on "pkg-config" => :build
+  # depends_on :x11
+  depends_on "libx11"
+  depends_on "libxcb"
+  depends_on "libxt"
+  depends_on "libxext"
+  depends_on "libxrender"
+
   depends_on "fontconfig"
   depends_on "freetype"
   depends_on "glib"
   depends_on "libpng"
   depends_on "lzo"
   depends_on "pixman"
-  depends_on :x11
+
+  uses_from_macos "zlib"
 
   def install
-    if build.head?
-      ENV["NOCONFIGURE"] = "1"
-      system "./autogen.sh"
-    end
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-gobject=yes
+      --enable-svg=yes
+      --enable-tee=yes
+      --enable-quartz-image
+      --enable-xcb=yes
+      --enable-xlib=yes
+      --enable-xlib-xrender=yes
+    ]
 
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--enable-gobject=yes",
-                          "--enable-svg=yes",
-                          "--enable-tee=yes",
-                          "--enable-quartz-image",
-                          "--enable-xcb=yes",
-                          "--enable-xlib=yes",
-                          "--enable-xlib-xrender=yes"
+    # if build.with? "x11"
+    #   args << "--enable-xcb=yes" << "--enable-xlib=yes" << "--enable-xlib-xrender=yes"
+    # else
+    #   args << "--enable-xcb=no" << "--enable-xlib=no" << "--enable-xlib-xrender=no"
+    # end
+
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
     system "make", "install"
   end
 
   test do
     (testpath/"test.c").write <<~EOS
       #include <cairo.h>
-
       int main(int argc, char *argv[]) {
-
         cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 600, 400);
         cairo_t *context = cairo_create(surface);
-
         return 0;
       }
     EOS
