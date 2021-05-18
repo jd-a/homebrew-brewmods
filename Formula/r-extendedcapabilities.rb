87741779409149bf1c73f3ef3171cbb0d390ca0c
jd-a <16205186+jd-a@users.noreply.github.com>
2021-05-18 09:43:19 +0200

    Update to 4.1.0

--- Formula/r-extendedcapabilities.rb
+++ Formula/r-extendedcapabilities.rb
@@ -1,8 +1,8 @@
 class RExtendedcapabilities < Formula
   desc "Software environment for statistical computing with extended capabilities"
   homepage "https://www.r-project.org/"
-  url "http://cran.r-project.org/src/base/R-4/R-4.0.5.tar.gz"
-  sha256 "0a3ee079aa772e131fe5435311ab627fcbccb5a50cabc54292e6f62046f1ffef"
+  url "http://cran.r-project.org/src/base/R-4/R-4.1.0.tar.gz"
+  sha256 "e8e68959d7282ca147360fc9644ada9bd161bab781bab14d33b8999a95182781"
 
   depends_on "pkg-config" => :build
   depends_on "gcc" # for gfortran
