b0a9198b4aeeb39d1dc5bb0351724a7875118b7a
jd-a <16205186+jd-a@users.noreply.github.com>
2023-11-01 00:10:36 +0100

    Update cairo-x11.rb

--- Formula/cairo-x11.rb
+++ Formula/cairo-x11.rb
@@ -1,69 +1,58 @@
-class CairoX11 < Formula
+class Cairo < Formula
   desc "Vector graphics library with cross-device output support"
   homepage "https://cairographics.org/"
   url "https://cairographics.org/releases/cairo-1.18.0.tar.xz"
   sha256 "243a0736b978a33dee29f9cca7521733b78a65b5418206fef7bd1c3d4cf10b64"
   license any_of: ["LGPL-2.1-only", "MPL-1.1"]
-  revision 1
+  head "https://gitlab.freedesktop.org/cairo/cairo.git", branch: "master"
 
-  # bottle do
-  #   sha256 "45f0b6aa6d76fa7806e1eeb066d6737033da3de74ac247a27735ff3a29b1b62b" => :big_sur
-  #   sha256 "eb04d54ac340a4954a178e99d3ea064913d3fe89184b1edd479c2a96260bb989" => :arm64_big_sur
-  #   sha256 "3d772a45e12f548338893e11cff0cd5c6a0a929bc214de8aa8cb6995c359bae9" => :catalina
-  #   sha256 "9ab59fee2cf7e7c331b95a9d5f026dbfdc03b6fa761304f729cdf87921c786bf" => :mojave
-  # end
-
-  head do
-    url "https://gitlab.freedesktop.org/cairo/cairo.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
+  livecheck do
+    url "https://cairographics.org/releases/?C=M&O=D"
+    regex(%r{href=(?:["']?|.*?/)cairo[._-]v?(\d+\.\d*[02468](?:\.\d+)*)\.t}i)
   end
 
-  keg_only "avoid conflicts with homebrew cairo"
+  bottle do
+    sha256 cellar: :any, arm64_sonoma:   "06c6aaadeca8f79c27867c56b8bb90fa9a7d00f84862cee7c837b611ffb8dbbc"
+    sha256 cellar: :any, arm64_ventura:  "e71518b5feb9f2c6a91152948fb8bb0492d7677581d9cd22f72e1a53e89753bb"
+    sha256 cellar: :any, arm64_monterey: "b4912ed29c6ef6796ae80480b8a806afeb55366d73661c5fe778500ada73ea7c"
+    sha256 cellar: :any, sonoma:         "18232de7a1880477f40f421262fa05f92278c7f494b3cabb1848dda92c545010"
+    sha256 cellar: :any, ventura:        "53fa7ded83d0f45fd7c8c25fedb970f1084bc4861f10988f36c2f2cfd6064552"
+    sha256 cellar: :any, monterey:       "a0368a0df2890afe6e66d1c45c55af4b2cd7eb8bdaf2b3e173fd711a2aad6fb6"
+    sha256               x86_64_linux:   "35cd3a9f81432449e9b0f457a20c8e94e0d2c804da0210a5deffe9e0b8549b4f"
+  end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "fontconfig"
   depends_on "freetype"
+  depends_on "glib"
   depends_on "libpng"
   depends_on "libx11"
   depends_on "libxcb"
   depends_on "libxext"
   depends_on "libxrender"
-  depends_on "libxt"
   depends_on "lzo"
   depends_on "pixman"
-  # depends_on "glib" => :optional
 
   uses_from_macos "zlib"
 
   def install
-    args = %W[
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --enable-svg=yes
-      --enable-tee=yes
-      --with-x
-      --enable-xlib=yes
-      --enable-xlib-xrender=yes
-      --enable-xlib-xcb=yes
-      --enable-xcb=yes
-      --enable-quartz=yes
-      --enable-quartz-image=yes
-      --enable-quartz-font=yes
-      --enable-ft=yes
-      --enable-fc=yes
+    args = %w[
+      -Dfontconfig=enabled
+      -Dfreetype=enabled
+      -Dpng=enabled
+      -Dglib=enabled
+      -Dxcb=enabled
+      -Dxlib=enabled
+      -Dzlib=enabled
+      -Dglib=enabled
     ]
+    args << "-Dquartz=enabled" if OS.mac?
 
-    # args << "--enable-gobject=yes" if build.with? "glib"
-
-    if build.head?
-      ENV["NOCONFIGURE"] = "1"
-      system "./autogen.sh", *args
-    end
-
-    system "./configure", *args
-    system "make", "install"
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   test do
