c8d3d85acaba599b7c376e3b6eee6d3c31a4855c
jd-a <16205186+jd-a@users.noreply.github.com>
2025-05-09 14:13:34 +0200

    Update vtracer.rb
    Actually the license is a MIT type license

--- Formula/vtracer.rb
+++ Formula/vtracer.rb
@@ -3,7 +3,7 @@ class Vtracer < Formula
   homepage "https://github.com/visioncortex/vtracer"
   url "https://github.com/visioncortex/vtracer/archive/refs/tags/0.6.4.zip"
   sha256 "20010bd11eb56a1f72e77da12c1fc03401533048747a3dd13b13f4e67e93d596"
-  license "Copyright (c) 2024 TSANG, Hao Fung"
+  license "MIT"
 
   depends_on "rust" => :build
 
