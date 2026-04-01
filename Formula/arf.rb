e36fb4bd19849c7d23df5d835fa4d488cb18e96a
jd-a <16205186+jd-a@users.noreply.github.com>
2026-04-01 10:57:48 +0200

    Create arf.rb
    initial commit

---
+++ Formula/arf.rb
@@ -0,0 +1,20 @@
+class Arf < Formula
+  desc "A modern R console written in Rust "
+  homepage "https://github.com/eitsupi/arf"
+  url "https://github.com/eitsupi/arf/archive/refs/tags/v0.2.6.zip"
+  sha256 "cda3aefa438d5b738d00a071249f3f9d53441a4c2a2bb68acc4418d574056f2b"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    # Build and install
+    system "cargo", "install",
+           *std_cargo_args
+  end
+
+  test do
+    # Version check
+    assert_match version.to_s, shell_output("#{bin}/arf --version")
+  end
+end
