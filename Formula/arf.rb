905cdc710e88f9f78a320f50fb80365d81a62f72
jd-a <16205186+jd-a@users.noreply.github.com>
2026-05-11 09:59:14 +0200

    Update arf to 0.3.3

--- Formula/arf.rb
+++ Formula/arf.rb
@@ -1,8 +1,8 @@
 class Arf < Formula
   desc "Modern R console written in Rust"
   homepage "https://github.com/eitsupi/arf"
-  url "https://github.com/eitsupi/arf/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "c2f7ac073d17063417e9bb026fd226dd39c31b350d3d26c305fe7ee149711f4a"
+  url "https://github.com/eitsupi/arf/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "54859e9d9517b2df1f0149bf9a0ac91e8d4874a1854b79f252c99a8bfce68392"
   license "MIT"
 
   depends_on "rust" => :build
